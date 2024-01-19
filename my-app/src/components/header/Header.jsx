1960ec13fcc15ad0cc8061cbc11ce7f9e9825060
dongyunkim96 <dongyunkim96@gmail.com>
2024-01-19 00:33:02 -0500

    header ver 1

--- my-app/src/components/header/Header.jsx
+++ my-app/src/components/header/Header.jsx
@@ -6,6 +6,19 @@ import {
     LeftContent,
     Title,
     AnimatedText,
+    GrayParagraph,
+    ButtonContainer,
+    HireMeButton,
+    DownloadButton,
+    ResumeButton,
+    IconListItem,
+    IconsContainer,
+    RightContainer,
+    ImageContainer,
+    MainImage,
+    ImageOverlay,
+    ThumbnailContainer,
+    PlayIcon,
 } from "./Header.styled.js";
 import {
     AiFillTwitterCircle,
@@ -39,83 +52,71 @@ const Header = () => {
                           wrapper='h2'
                           repeat={Infinity}
                         />
-                        <p className=' text-[1.1rem] font-medium w-3/4 md:w-full text-gray-600 sm:text-[.95rem]'>
+                        <GrayParagraph>
                             This is section 1
-                        </p>
+                        </GrayParagraph>
                     </Information>
-                    <div data-aos="fade-up" className='buttons flex gap-5'>
-                        <a 
-                          href=" "
-                          className='bg-black text-[1rem] text-white px-10 py-2 sm:px-8 rounded-lg font-bold hover:text-yellow-500'
-                        >
+                    <ButtonContainer data-aos="fade-up">
+                        <HireMeButton href=" ">
                             <span>Hire Me</span>
-                        </a>
-                        <a
-                          href=" "
-                          className='flex items-center gap-2 border-text-[1rem] bg-white border-black px-7 py-2 sm:px-6 rounded-lg font-bold hover:text-yellow-500'
-                          download
-                        >
-                            <div className='flex items-center gap-1'>
+                        </HireMeButton>
+                        <DownloadButton href="" download>
+                            <ResumeButton>
                                 Resume <FiDownload />
-                            </div>
-                        </a>
-                    </div>
-                    <div className='icons flex mt-5'>
+                            </ResumeButton>
+                        </DownloadButton>
+                    </ButtonContainer>
+                    <IconsContainer>
                         <ul
                           data-aos="fade-up"
                           data-aos-duration="1500"
-                          className='flex gap-5'
                         >
-                            <li>
+                            <IconListItem>
                                 <a href=' '>
-                                    <AiFillGithub className="h-[1.8rem] w-[1.8rem] text-xl hover:scale-125" />
+                                    <AiFillGithub />
                                 </a>
-                            </li>
-                            <li>
+                            </IconListItem>
+                            <IconListItem>
                                 <a href=' '>
-                                    <FaLinkedin className="h-[1.8rem] w-[1.8rem] text-xl hover:scale-125" />
+                                    <FaLinkedin />
                                 </a>
-                            </li>
-                            <li>
+                            </IconListItem>
+                            <IconListItem>
                                 <a href=' '>
-                                    <AiFillInstagram className="h-[1.8rem] w-[1.8rem] text-xl hover:scale-125" />
+                                    <AiFillInstagram />
                                 </a>
-                            </li>
-                            <li>
+                            </IconListItem>
+                            <IconListItem>
                                 <a href=' '>
-                                    <FaFacebook className="h-[1.8rem] w-[1.8rem] text-xl hover:scale-125" />
+                                    <FaFacebook />
                                 </a>
-                            </li>
-                            <li>
+                            </IconListItem>
+                            <IconListItem>
                                 <a href=' '>
-                                    <AiFillTwitterCircle className="h-[1.8rem] w-[1.8rem] text-xl hover:scale-125" />
+                                    <AiFillTwitterCircle />
                                 </a>
-                            </li>
+                            </IconListItem>
                         </ul>
-                    </div>
+                    </IconsContainer>
                 </LeftContent>
-                <div className='right top-5 flex-1 flex items-center justify-center md:items-end sm:items-end'>
-                    <div className='relative h-[88%] w-fit flex items-center sm:items-end'>
-                        <img 
+                <RightContainer>
+                    <ImageContainer>
+                        <MainImage
                           data-aos="fade-up"
-                          className='h-[90%] w-full object-cover md:h-[95%] md:m-auto sm:m-0'
                           src={" "}
                           alt='my photo'
                         />
-                        <div className='absolute bottom-10 md:bottom-3 right-8 md:right-2'>
-                            <div data-aos="zoom-in" data-aos-duration="1000" className='relative cursor-pointer'>
+                        <ImageOverlay>
+                            <ThumbnailContainer data-aos="zoom-in" data-aos-duration="1000">
                                 <img
-                                  className='w-[135px] md:w-[90px] circle-text'
                                   src='Front-end developer image here'
-                                  alt='Front-end-image'
+                                  alt=' '
                                 />
-                                <FaPlay
-                                  className='text-black absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                                <PlayIcon />
+                            </ThumbnailContainer>
+                        </ImageOverlay>
+                    </ImageContainer>
+                </RightContainer>
             </Content>
         </Container>
     );
--- my-app/src/components/header/Header.styled.js
+++ my-app/src/components/header/Header.styled.js
@@ -1,5 +1,6 @@
 import { TypeAnimation } from "react-type-animation";
 import { styled } from "styled-components";
+import { FaPlay } from "react-icons/fa"
 
 export const Container = styled.div`
 background: linear-gradient(to right, #a0f28c, #87ceeb);
@@ -60,4 +61,156 @@ export const AnimatedText = styled(TypeAnimation)`
   @media (max-width: 767px) {
     font-size: 3x1;
   }
+`;
+
+export const GrayParagraph = styled.p`
+  font-size: 1.1rem;
+  font-weight: medium;
+  width: 75%;
+  color: #717171;
+
+  @media (max-width: 767px) {
+    font-size: 0.95rem;
+    width: 100%;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  gap: 5px;
+`;
+
+export const HireMeButton = styled.a`
+  background-color: #000;
+  color: #fff;
+  font-size: 1rem;
+  padding: 10px;
+  border-radius: 8px;
+  font-weight: bold;
+  transition: color 0.3s;
+
+  &:hover {
+    color: #ffed4a;
+  }
+
+  @media (max-width: 767px) {
+    padding: 8px;
+    font-size: 0.9rem;
+  }
+`;
+
+export const DownloadButton = styled.a`
+  display: flex;
+  align-items: center;
+  gap: 2px;
+  border: 1px solid #000;
+  background-color: #fff;
+  padding: 7px;
+  border-radius: 8px;
+  font-weight: bold;
+  transition: color 0.3s;
+
+  &:hover {
+    color: #ffed4a;
+  }
+
+  @media (max-width: 767px) {
+    padding: 6px;
+    font-size: 0.9rem;
+  }
+`;
+
+export const ResumeButton = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const IconsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 5px;
+
+  ul {
+    display: flex;
+    gap: 5px;
+  }
+`;
+
+export const IconListItem = styled.li`
+  a {
+    display: block;
+    text-decoration: none;
+    color: inherit;
+    height: 1.8rem;
+    width: 1.8rem;
+    font-size: 1.6rem;
+
+    &:hover {
+        transform: scale(1.25);
+    }
+  }
+`;
+
+export const RightContainer = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  @media (min-width: 767px) {
+    align-items: flex-end;
+  }
+`;
+
+export const ImageContainer = styled.div`
+  position: relative;
+  height: 88%;
+  width: fit-content;
+  display: flex;
+  align-items: center;
+
+  @media (min-width: 768px) {
+    align-items: flex-end;
+  }
+`;
+
+export const MainImage = styled.img`
+  height: 90%;
+  width: 100%;
+  object-fit: cover;
+
+  @media (min-width: 768px) {
+    height: 95%;
+    margin: auto;
+  }
+`;
+
+export const ImageOverlay = styled.div`
+  position: absolute;
+  bottom: 10%;
+  right: 8%;
+`;
+
+export const ThumbnailContainer = styled.div`
+  position: relative;
+  cursor: pointer;
+
+  img {
+    width: 135px;
+
+    @media (min-width: 768px) {
+        width: 90px;
+    }
+  }
+`;
+
+export const PlayIcon = styled(FaPlay)`
+  color: #000;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;

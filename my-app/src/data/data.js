bee6c65a668331382b44cf98337cfe85bd7a62ca
dongyunkim96 <dongyunkim96@gmail.com>
2024-04-18 14:59:24 -0400

    works data add

--- my-app/src/data/data.js
+++ my-app/src/data/data.js
@@ -1,11 +1,13 @@
+import CSSexp from "../assets/CSSexp.png";
+
 export const data = [
     {
         id: 1,
-        img: '',
-        title: '',
-        desc: '',
-        link: '',
-        git: '',
+        img: CSSexp,
+        title: '100 types of CSS',
+        desc: 'You can check out 100 different kinds of CSS works.',
+        link: 'https://dongyunkim96.github.io/CSS-Experience/',
+        git: 'https://github.com/dongyunkim96/CSS-Experience',
     },
     {
         id: 2,
